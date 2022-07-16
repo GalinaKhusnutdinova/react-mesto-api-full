5426e69b2d46c81f31a659193cdcd0bc1b1945b5
Galina Khusnutdinova <khusnutdinova.gal@yandex.ru>
2022-07-16 17:10:13 +0300

    ПР15

--- frondend/src/utils/api.js
+++ frondend/src/utils/api.js
@@ -77,11 +77,10 @@ class Api {
     return Promise.reject(`Ошибка: ${res.status}`);
   }
 }
-// const baseUrl = `${'http://api.khusnutdinova.student.nomoredomains.xyz' || 'http://localhost:3001'}` 
-
+const baseUrl = `${'http://api.khusnutdinova.student.nomoredomains.xyz' || 'http://localhost:3001'}` 
 
 export const api = new Api({
-  baseUrl: `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3001'}`,
+  baseUrl: baseUrl,
   headers: {
     authorization: 'Bearer ' + localStorage.getItem('jwt'),
     "Content-Type": "application/json",
